ddfa016f0bea67df60ce28e4ed7243449b6969dd
Faheem Zunjani <faheemzunjani@gmail.com>
2017-02-27 02:03:39 +0530

    Add documentation HTML files

---
+++ Q1-2/docs/search/all_4.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['essential',['essential',['../classdata_1_1gift__basket.html#a8c973e8953a265c2dbf4eb8f2310c33c',1,'data::gift_basket']]],
+  ['essential_5fgift',['essential_gift',['../classdata_1_1essential__gift.html',1,'data::essential_gift'],['../classdata_1_1essential__gift.html#ae01011916dc030a3f0700bac1499b803',1,'data::essential_gift::essential_gift()']]]
+];
---
+++ Q1-2/docs/search/variables_3.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['luxury',['luxury',['../classdata_1_1gift__basket.html#ab088caacb7e2676add1b34814b51a206',1,'data::gift_basket']]]
+];
