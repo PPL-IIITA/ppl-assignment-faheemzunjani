70cf1e257c1c1f461097d5e2d617bb134b51df55
Faheem Zunjani <faheemzunjani@gmail.com>
2017-02-27 01:59:56 +0530

    Removed docs from master branch

--- Q1-2/docs/search/functions_b.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['set_5fcompatibility',['set_compatibility',['../classdata_1_1couple.html#a6e65b66f24c19f01a5829c875b2cd7c6',1,'data::couple']]],
-  ['set_5fhappiness',['set_happiness',['../classdata_1_1boy.html#a0ad01fab328a2292fc6bc49c81ee3621',1,'data::boy::set_happiness()'],['../classdata_1_1choosy__girl.html#a72b4eb1d9909ad2d9c1c54a6f1b7bf57',1,'data::choosy_girl::set_happiness()'],['../classdata_1_1couple.html#a04ea13b140ccd7075c472e882fdfcb1c',1,'data::couple::set_happiness()'],['../classdata_1_1desperate__girl.html#aadc4cbba7e04074fbb231fa9373c33c4',1,'data::desperate_girl::set_happiness()'],['../classdata_1_1geek__boy.html#ac467fe30b8d749a949aee0a09713900b',1,'data::geek_boy::set_happiness()'],['../classdata_1_1generous__boy.html#abfa4f1f730374286699d636033ec5f87',1,'data::generous_boy::set_happiness()'],['../classdata_1_1girl.html#a6ee681e8b5db767446fb1ce27e606886',1,'data::girl::set_happiness()'],['../classdata_1_1miser__boy.html#aba926d1408efbccb52ec21e7f6b600a6',1,'data::miser_boy::set_happiness()'],['../classdata_1_1normal__girl.html#a1030cca3dbe936825b99f8dcbe038055',1,'data::normal_girl::set_happiness()']]]
-];
--- Q1-2/docs/search/variables_1.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['essential',['essential',['../classdata_1_1gift__basket.html#a8c973e8953a265c2dbf4eb8f2310c33c',1,'data::gift_basket']]]
-];
