e1ea5feb3cd260a0fa3ab38f87576778a64d9e9d
Faheem Zunjani <faheemzunjani@gmail.com>
2017-02-27 01:51:03 +0530

    Add documentation HTML files

---
+++ Q1-2/docs/search/all_c.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['utility',['utility',['../namespaceutility.html',1,'utility'],['../classdata_1_1gift__basket.html#a864284ebd4d0c9c41d9fed9ae0192225',1,'data::gift_basket::utility()']]],
+  ['utility_5fgift',['utility_gift',['../classdata_1_1utility__gift.html',1,'data::utility_gift'],['../classdata_1_1utility__gift.html#ad9279613094272e2d074506cada307e7',1,'data::utility_gift::utility_gift()']]]
+];
