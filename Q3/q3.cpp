3d41cc41ee12d130d6af8ebdb351590efb81920d
Faheem Zunjani <faheemzunjani@gmail.com>
2017-03-28 16:30:13 +0530

    Finish Q3

--- Q3/q3.cpp
+++ Q3/q3.cpp
@@ -1,4 +1,4 @@
-#include "./library/variables_q2.hpp"
+#include "./library/variables_q3.hpp"
 #include "./library/algorithms.hpp"
 #include "./library/utility.hpp"
 
@@ -6,6 +6,10 @@ using namespace data;
 
 int main()
 {
+	utility::read_boys_data(geek_boys, generous_boys, miser_boys);
+	utility::read_girls_data(normal_girls, choosy_girls, desperate_girls);
+	algorithms::make_couples(geek_boys, generous_boys, miser_boys, 
+							 normal_girls, choosy_girls, desperate_girls);
 	utility::read_couples_data(couples);
 	utility::read_gifts_data(essential_gifts, luxury_gifts, utility_gifts);
 
