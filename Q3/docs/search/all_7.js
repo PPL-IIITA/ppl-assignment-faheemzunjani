9b3ee568bcc2c59fe6c8b03af892adaa6418f322
Faheem Zunjani <faheemzunjani@gmail.com>
2017-03-28 14:57:36 +0530

    Added reusable files from previous question

---
+++ Q3/docs/search/all_7.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['make_5fcouples',['make_couples',['../namespacealgorithms.html#ab00517864bce4f01b245156a450a6844',1,'algorithms']]],
+  ['miser_5fboy',['miser_boy',['../classdata_1_1miser__boy.html',1,'data::miser_boy'],['../classdata_1_1miser__boy.html#afc93380f5d69f5f50d4220957ba832a7',1,'data::miser_boy::miser_boy()']]]
+];
---
+++ Q3/docs/search/variables_2.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['gifts',['gifts',['../classdata_1_1couple.html#a057d1c2e7bda328e9656adf4c07742a4',1,'data::couple']]]
+];
