aef67c6bbd32c1beba77752ad57e9e02734ba315
Faheem Zunjani <faheemzunjani@gmail.com>
2017-04-10 01:26:28 +0530

    Added documentation fo master branch

---
+++ Q7/docs/search/all_10.js
@@ -0,0 +1,12 @@
+var searchData=
+[
+  ['utility',['utility',['../namespaceutility.html',1,'utility'],['../classdata_1_1gift__basket.html#a864284ebd4d0c9c41d9fed9ae0192225',1,'data::gift_basket::utility()']]],
+  ['utility_2ecpp',['utility.cpp',['../utility_8cpp.html',1,'']]],
+  ['utility_2ehpp',['utility.hpp',['../utility_8hpp.html',1,'']]],
+  ['utility_5fclass',['utility_class',['../classdata_1_1utility__gift.html#a4240f2931c566bf8989cf3f51cc21d75',1,'data::utility_gift']]],
+  ['utility_5fgift',['utility_gift',['../classdata_1_1utility__gift.html',1,'data::utility_gift'],['../classdata_1_1utility__gift.html#ad9279613094272e2d074506cada307e7',1,'data::utility_gift::utility_gift()']]],
+  ['utility_5fgift_2ecpp',['utility_gift.cpp',['../utility__gift_8cpp.html',1,'']]],
+  ['utility_5fgift_2ehpp',['utility_gift.hpp',['../utility__gift_8hpp.html',1,'']]],
+  ['utility_5fgifts',['utility_gifts',['../variables__q3_8hpp.html#ab600bb44bdf895e307b1736f0a7b6262',1,'utility_gifts():&#160;variables_q3.hpp'],['../variables__q4_8hpp.html#ab600bb44bdf895e307b1736f0a7b6262',1,'utility_gifts():&#160;variables_q4.hpp']]],
+  ['utility_5fvalue',['utility_value',['../classdata_1_1utility__gift.html#a181d59f29773b1799d7199ef02f1259a',1,'data::utility_gift']]]
+];
---
+++ Q7/docs/search/searchdata.js
@@ -0,0 +1,30 @@
+var indexSectionsWithContent =
+{
+  0: "abcdeghilmnpqrstuv",
+  1: "bcdeglmnu",
+  2: "adu",
+  3: "abcdeglmnquv",
+  4: "abcdeglmnprstu",
+  5: "abcdeghilmnprsuv"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "classes",
+  2: "namespaces",
+  3: "files",
+  4: "functions",
+  5: "variables"
+};
+
+var indexSectionLabels =
+{
+  0: "All",
+  1: "Classes",
+  2: "Namespaces",
+  3: "Files",
+  4: "Functions",
+  5: "Variables"
+};
+
