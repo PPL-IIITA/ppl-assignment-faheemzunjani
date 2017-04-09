a687495dea49cf968e1caf37e3dc22f991ada82c
Faheem Zunjani <faheemzunjani@gmail.com>
2017-04-09 22:41:15 +0530

    Added documentation for Q4

---
+++ Q4/docs/search/all_8.js
@@ -0,0 +1,8 @@
+var searchData=
+[
+  ['luxury',['luxury',['../classdata_1_1gift__basket.html#ab088caacb7e2676add1b34814b51a206',1,'data::gift_basket']]],
+  ['luxury_5fgift',['luxury_gift',['../classdata_1_1luxury__gift.html',1,'data::luxury_gift'],['../classdata_1_1luxury__gift.html#a12823b355bb8e3c49fe09d333e393cb8',1,'data::luxury_gift::luxury_gift()']]],
+  ['luxury_5fgift_2ecpp',['luxury_gift.cpp',['../luxury__gift_8cpp.html',1,'']]],
+  ['luxury_5fgift_2ehpp',['luxury_gift.hpp',['../luxury__gift_8hpp.html',1,'']]],
+  ['luxury_5fgifts',['luxury_gifts',['../variables__q4_8hpp.html#a17cb2c4e9522180f4bacb358174e4d3e',1,'variables_q4.hpp']]]
+];
---
+++ Q4/docs/search/files_9.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['q4_2ecpp',['q4.cpp',['../q4_8cpp.html',1,'']]]
+];
