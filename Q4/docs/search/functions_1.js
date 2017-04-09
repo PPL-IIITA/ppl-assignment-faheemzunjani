cb66bc7c34900e05afdabe034550c53c141e4f43
Faheem Zunjani <faheemzunjani@gmail.com>
2017-04-09 22:41:15 +0530

    Added documentation for Q4

---
+++ Q4/docs/search/functions_1.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['boy',['boy',['../classdata_1_1boy.html#a201c0ec0ef74fd430be4850568c7f003',1,'data::boy::boy()'],['../classdata_1_1boy.html#abfe216d9b39115dc2a6a073a90b0f7a6',1,'data::boy::boy(std::string name_id, int attractiveness, int min_attractiveness_req, double budget, int iq, std::string commit_type, std::string commit_to)']]],
+  ['breakup_5fleast_5fk_5fhappiest_5fcouples',['breakup_least_k_happiest_couples',['../namespacealgorithms.html#a066eb6b88a6067db0324f6fe0e516982',1,'algorithms']]]
+];
