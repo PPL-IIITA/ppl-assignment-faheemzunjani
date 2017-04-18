25d0923971510e530eb606e8f4b567522d70d838
Faheem Zunjani <faheemzunjani@gmail.com>
2017-04-18 19:41:44 +0530

    Added gifting exception

--- Q11/gifting_exception.cpp
+++ Q11/gifting_exception.cpp
@@ -10,10 +10,10 @@ int main()
 	utility::read_girls_data(normal_girls, choosy_girls, desperate_girls);
 	algorithms::make_couples(geek_boys, generous_boys, miser_boys, 
 							 normal_girls, choosy_girls, desperate_girls);
-	//utility::read_couples_data(couples);
-	//utility::read_gifts_data(essential_gifts, luxury_gifts, utility_gifts);
+	utility::read_couples_data(couples);
+	utility::read_gifts_data(essential_gifts, luxury_gifts, utility_gifts);
 
-	//algorithms::gifting(couples, essential_gifts, luxury_gifts, utility_gifts);
+	algorithms::gifting(couples, essential_gifts, luxury_gifts, utility_gifts);
 	
 	//utility::print_k_happiest_couples(couples);
 	//utility::print_k_compatibile_couples(couples);
